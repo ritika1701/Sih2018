601e60bdcb2f367756a3a547ecc9c63394fb8c28
pankhuri1712 <srivastavapankhuri.12@gmail.com>
2018-03-29 13:32:14 +0530

    model.py change

--- dashboard/models.py
+++ dashboard/models.py
@@ -8,7 +8,7 @@ class School(models.Model):
     schoolname=models.CharField(max_length=100)
     def __str__(self):
         return self.school_code
-
+#Registration of school
 class SignUp(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     school_code=models.OneToOneField(School,on_delete=models.CASCADE)
@@ -19,42 +19,48 @@ class SignUp(models.Model):
     def __str__(self):
         return self.Principal
 
-
+#details of student
 class Details(models.Model):
     st_code = models.IntegerField(max_length=100,default=0,primary_key=True)
     name = models.CharField(max_length=100)
     college = models.CharField(max_length=1000)
+    Location = models.TextField()
     email = models.EmailField(max_length=70, unique= True)
 
     def __str__(self):
         return "{}".format(self.college)
-
+# academic data and score
 class Academics(models.Model):
     english= models.IntegerField(max_length=10,default=0)
     maths= models.IntegerField(max_length=10,default=0)
-    science= models.IntegerField(max_length=10,default=0)
-    evs= models.IntegerField(max_length=10,default=0)
-    sst= models.IntegerField(max_length=10,default=0)
+    science= models.IntegerField(default=0)
+    evs= models.IntegerField(default=0)
+    sst= models.IntegerField(default=0)
+    Academic_score = models.IntegerField(default=0)
     student = models.ForeignKey(Details,on_delete=models.CASCADE)
 
-
+# sports data and score
 class sports(models.Model):
-    sports_played =models.CharField(max_length=100)
-    matches =models.IntegerField(max_length=1000,default=0)
-    semi =models.IntegerField(max_length=1000,default=0)
-    final =models.IntegerField(max_length=1000,default=0)
-    won=models.IntegerField(max_length=1000,default=0)
+    sports_played = models.CharField(max_length=100)
+    matches =models.IntegerField(default=0)
+    semi= models.IntegerField(default=0)
+    final= models.IntegerField(default=0)
+    won=models.IntegerField(default=0)
+    sport_score = models.IntegerField(default=0)
     student = models.ForeignKey(Details,on_delete=models.CASCADE)
 
 
     def __str__(self):
         return self.sports_played
-
+# activity data and score
 class extra_curricular(models.Model):
     ativities =models.CharField(max_length=100)
-    total =models.IntegerField(max_length=1000,default=0)
-    won =models.IntegerField(max_length=1000,default=0)
+    Inter_played =models.IntegerField(default=0)
+    Inter_won = models.IntegerField(default=0)
+    Intra_played= models.IntegerField(default=0)
+    Intra_won = models.IntegerField(default=0)
     student = models.ForeignKey(Details,on_delete=models.CASCADE)
+    activity_score = models.IntegerField(default=0)
 
 
     def __str__(self):
