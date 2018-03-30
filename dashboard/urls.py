f670a5db8e25cc7f6e80c87715da406ffe156635
ritika1701 <ritikamittal1701@gmail.com>
2018-03-30 13:05:01 +0530

    login,register updated

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token
-from .views import  AuthRegister
+from .views import  AuthRegister,Profile
 
 urlpatterns=[
   url(r'^login/',obtain_jwt_token),
   url(r'^register/$', AuthRegister.as_view()),
+  url(r'^viewprofile/(?P<username>[A-Za-z0-9]+)/$', Profile.as_view()),
   ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,9 +2,11 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from .serializers import AccountSerializer
+from .serializers import AccountSerializer,AccountGetSerializer
 from .models import SignUp
 from django.http import Http404
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 
 class AuthRegister(APIView):
     """
@@ -20,3 +22,17 @@ class AuthRegister(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class Profile(APIView):
+    serializer_class = AccountGetSerializer
+    permission_classes = (AllowAny,)
+
+    def get_object(self, username):
+        try:
+            return User.objects.get(username=username)
+        except Account.DoesNotExist:
+            raise Http404
+
+    def get(self,request,username,format=None):
+        account = self.get_object(username)
+        serializer=self.serializer_class(account)
+        return Response(serializer.data)
