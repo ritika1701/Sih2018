c19ba95a4cccd93616e5ae3678ba776be38c455e
pankhuri1712 <srivastavapankhuri.12@gmail.com>
2018-03-30 11:18:24 +0530

    models.py changed

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class School(models.Model):
-    school_code=models.CharField(max_length=100)
+    schoolcode=models.CharField(max_length=100)
     schoolname=models.CharField(max_length=100)
     def __str__(self):
         return self.school_code
@@ -12,7 +12,6 @@ class School(models.Model):
 class SignUp(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     school_code=models.OneToOneField(School,on_delete=models.CASCADE)
-    Principal =models.CharField(max_length=100)
     email= models.EmailField(max_length=70, unique= True)
     contact=models.CharField(max_length=10)
     Location = models.TextField(null=True)
@@ -22,17 +21,21 @@ class SignUp(models.Model):
 
 #details of student
 class Details(models.Model):
-    st_code = models.IntegerField(default=0,primary_key=True)
-    name = models.CharField(max_length=100)
-    college = models.CharField(max_length=1000)
-
-    email = models.EmailField(max_length=70, unique= True)
+    rollno = models.CharField(max_length=100,primary_key =True)
+    student_name = models.CharField(max_length=100)
+    school = models.ForeignKey(School)
+    gender = models.CharField(max_length=10)
+    dob= models.Datefield(max_length=8)
+    teacher_remark = models.TextField(null=True)
+    preferred = models.CharField(max_length=100)
+    marksheet = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
+   
 
     def __str__(self):
         return "{}".format(self.college)
 # academic data and score
 class Academics(models.Model):
-    english= models.IntegerField(default=0)
+    eng= models.IntegerField(default=0)
     maths= models.IntegerField(default=0)
     science= models.IntegerField(default=0)
     evs= models.IntegerField(default=0)
