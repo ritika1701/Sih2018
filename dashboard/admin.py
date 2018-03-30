1b0f853485417627bde70469d3a1b3e07587c568
ritika1701 <ritikamittal1701@gmail.com>
2018-03-30 14:47:12 +0530

    Changed register

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import SignUp
+from .models import SignUp,School
 
 # Register your models here.
 admin.site.register(SignUp)
+admin.site.register(School)
--- sih18/settings.py
+++ sih18/settings.py
@@ -51,6 +51,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
 ROOT_URLCONF = 'sih18.urls'
 
 TEMPLATES = [
