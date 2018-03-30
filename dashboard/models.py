b78319ec9a4e56af927b05ecbb03544eba66ec29
root <nishtha1896@gmail.com>
2018-03-30 12:15:41 +0530

    few changes

--- dashboard/models.py
+++ dashboard/models.py
@@ -6,40 +6,36 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class School(models.Model):
+    schoolcode = models.CharField(max_length=100)
+    schoolname = models.CharField(max_length=100)
 
-    schoolcode=models.CharField(max_length=100)
-    schoolname=models.CharField(max_length=100)
+    def __str__(self):
+        return self.schoolcode
+
+
+# Registration of school
+class SignUp(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    school_code = models.OneToOneField(School, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=70, unique=True)
+    contact = models.CharField(max_length=10)
+
+    Location = models.TextField(null=True)
 
     def __str__(self):
         return self.school_code
 
 
-# Registration of school
-class SignUp(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    school_code=models.OneToOneField(School,on_delete=models.CASCADE)
-    email= models.EmailField(max_length=70, unique= True)
-    contact=models.CharField(max_length=10)
-
-    Location = models.TextField(null=True)
-
-    def __str__(self):
-        return self.Principal
-
-
 # details of student
 class Details(models.Model):
-
-    rollno = models.CharField(max_length=100,primary_key =True)
+    rollno = models.CharField(max_length=100, primary_key=True)
     student_name = models.CharField(max_length=100)
     school = models.ForeignKey(School)
     gender = models.CharField(max_length=10)
-    dob= models.Datefield(max_length=8)
+    dob = models.Datefield(max_length=8)
     teacher_remark = models.TextField(null=True)
     preferred = models.CharField(max_length=100)
-    marksheet = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
-   
-
+    marksheet = models.ImageField(upload_to='pic_folder/', default='pic_folder/None/no-img.jpg')
 
     def __str__(self):
         return "{}".format(self.college)
@@ -47,12 +43,11 @@ class Details(models.Model):
 
 # academic data and score
 class Academics(models.Model):
-
-    eng= models.IntegerField(default=0)
-    maths= models.IntegerField(default=0)
-    science= models.IntegerField(default=0)
-    evs= models.IntegerField(default=0)
-    sst= models.IntegerField(default=0)
+    eng = models.IntegerField(default=0)
+    maths = models.IntegerField(default=0)
+    science = models.IntegerField(default=0)
+    evs = models.IntegerField(default=0)
+    sst = models.IntegerField(default=0)
 
     Academic_score = models.IntegerField(default=0)
     student = models.ForeignKey(Details, on_delete=models.CASCADE)
