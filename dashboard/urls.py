94a6bab523c7ee5c00fc13de90c6de7e54ec3add
ritika1701 <ritikamittal1701@gmail.com>
2018-03-31 11:41:35 +0530

    jwt

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token
 from .views import  AuthRegister,Profile,StudentRegister,OtpRegister
-from rest_framework_jwt.views import verify_jwt_token
+
 
 
 urlpatterns=[
   url(r'^login/',obtain_jwt_token),
   url(r'^register/$', AuthRegister.as_view()),
-  url(r'^auth-jwt-verify/', verify_jwt_token),
+
   url(r'^studentportal/',StudentRegister.as_view()),
   url(r'^otp/',OtpRegister.as_view()),
   url(r'^viewprofile/(?P<username>[A-Za-z0-9]+)/$', Profile.as_view()),
